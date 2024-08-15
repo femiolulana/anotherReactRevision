6261cb256c9a91234b5fc5c4f84f6041bce1e40b
femi <Femi.Olulana@liontechacademy.com>
2024-08-15 19:25:40 -0400

    more on listing

--- my-react-app/src/list.jsx
+++ my-react-app/src/list.jsx
@@ -1,8 +1,15 @@
 import propTypes from "prop-types"
 function List (props){
 
-    const fruits = ['apple','orange', 'pear',"pear",'banana',"coconut"];
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)  
+    const fruits = [{name:'apple',calories:95},
+                    {name:'orange',calories:62}, 
+                    {name:'pear',calories:101},
+                    {name:"pear",calories:105},
+                    {name:'banana',calories:105},
+                    {name:"coconut",calories:283 }
+                ];
+    fruits.sort();
+    const listItems = fruits.map(fruit => <li>{fruit.calories}</li>)  
     return (
         <>
             <ul>{listItems}</ul>
